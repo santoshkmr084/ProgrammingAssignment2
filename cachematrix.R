8c987fcf870b5e9548c84f5fe2dddcab8e8d3c52
“santoshkmr084” <santoshkmr084@gmail.com>
2015-07-27 02:05:23 +0530

    Placed the code for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,30 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL # sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
+    y <- NULL 
+    setmatrix <- function(y) { #set the value of the matrix
+        x <<- y ## caches the inputted matrix so that cacheSolve can check whether it has changed (note this is within the setmatrix function)
+        m <<- NULL # # sets the value of m (the matrix inverse if used cacheSolve) to NULL
+    }
+    
+    list(setmatrix = setmatrix, getmatrix = getmatrix, # creates a list to house the four functions
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv() # get the inversed matrix from object x
+    # it will be null if uncalculated, remember the first line "xinv <- NULL" in the previous function
+    if(!is.null(m)) { # if the inversion result is there
+        message("getting cached data")
+        return(m) # return the calculated inversion
+    }
+    data <- x$get() # if not, we do x$get to get the matrix object
+    m <- solve(data) # we solve it
+    x$setInv(m) # we then set it to the object
+    m # return the solved result
 }
